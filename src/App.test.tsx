3ed103bef462cfbae76b4e93dca21464f7d2deb8
Nabeel <nabeelfarid@gmail.com>
2021-03-17 05:11:52 +0500

    Timer Component with Unit Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,199 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import { unmountComponentAtNode } from 'react-dom';
+
+describe('Timer App', () => {
+
+  // Fake timers using Jest
+  beforeEach(() => {
+    jest.useFakeTimers();
+    
+  })
+  // Running all pending timers and switching to real timers using Jest
+  afterEach(() => {
+    jest.clearAllTimers();
+    act(() => {
+      jest.runOnlyPendingTimers()
+      jest.useRealTimers()
+    });
+  })
+
+  test('Should have a button to Start the timer', () => {
+
+    //Act
+    render(<App />)
+
+    //Assert
+    expect(screen.getByRole('button', { name: /start/i })).toBeInTheDocument();
+
+  });
+
+  test('SHOULD have a button to Stop the timer', () => {
+
+    //Act
+    render(<App />)
+
+    //Assert
+    expect(screen.getByRole('button', { name: /stop/i })).toBeInTheDocument();
+
+  });
+
+  test('SHOULD have a button to Reset the timer', () => {
+
+    //Act
+    render(<App />)
+
+    //Assert
+    expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument();
+
+  });
+
+  test('SHOULD have a disabled Stop button on load', () => {
+
+    //Act
+    render(<App />)
+
+    //Assert
+    expect(screen.getByRole('button', { name: /start/i })).toBeEnabled();
+    expect(screen.getByRole('button', { name: /stop/i })).toBeDisabled();
+
+  });
+
+  test('WHEN Timer is started, SHOULD disable Start button AND enable Stop button  ', () => {
+
+    //Act
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+
+    //Assert
+    expect(screen.getByRole('button', { name: /start/i })).toBeDisabled();
+    expect(screen.getByRole('button', { name: /stop/i })).toBeEnabled();
+
+  });
+
+  test('WHEN Timer is stopped, SHOULD enable Start button AND disable Stop button  ', () => {
+
+    //Act
+    render(<App />,)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+    userEvent.click(screen.getByRole('button', { name: /stop/i }))
+
+    //Assert
+    expect(screen.getByRole('button', { name: /start/i })).toBeEnabled();
+    expect(screen.getByRole('button', { name: /stop/i })).toBeDisabled();
+
+  });
+
+  test('WHEN Timer is reset after starting, SHOULD enable Start button AND disable Stop button  ', () => {
+
+    //Act
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+    userEvent.click(screen.getByRole('button', { name: /reset/i }))
+
+    //Assert
+    expect(screen.getByRole('button', { name: /start/i })).toBeEnabled();
+    expect(screen.getByRole('button', { name: /stop/i })).toBeDisabled();
+
+  });
+
+  test('WHEN Timer is loaded, it is set to 25 min and 00 sec  ', () => {
+
+    //Act
+    render(<App />)
+
+    //Assert
+    expect(screen.getByRole('timer')).toHaveTextContent(/25:00/i);
+    // expect(screen.getByRole('heading', { name: `Welcome nabeel!` })).toBeInTheDocument();
+
+  });
+
+  test('WHEN Timer is started, AFTER 5 secs it should count down to 24:55  ', async () => {
+    //Act
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+
+    act(() => {
+      jest.advanceTimersByTime(5000);
+    });
+
+    //Assert
+    expect(await screen.findByRole('timer')).toHaveTextContent(/24:55/i);
+
+  });
+
+  test('AFTER 60 secs timer should count down to 24:00  ', async () => {
+    //Act
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+
+    act(() => {
+      jest.advanceTimersByTime(60000);
+    });
+
+    //Assert
+    expect(await screen.findByRole('timer')).toHaveTextContent(/24:00/i);
+
+  });
+
+  test('AFTER 25 min timer should reach 00:00  ', async () => {
+    //Act
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+
+    act(() => {
+      jest.advanceTimersByTime(60000 * 25);
+    });
+
+    //Assert
+    expect(await screen.findByRole('timer')).toHaveTextContent(/00:00/i);
+
+  });
+
+  test('AFTER passing over 25 min timer should stop at 00:00  ', async () => {
+    //Act
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+
+    act(() => {
+      jest.advanceTimersByTime((60000 * 25) + 1000);
+    });
+
+    //Assert
+    expect(await screen.findByRole('timer')).toHaveTextContent(/00:00/i);
+    expect(await screen.findByRole('button', { name: /start/i })).toBeEnabled();
+    expect(await screen.findByRole('button', { name: /stop/i })).toBeDisabled();
+
+  });
+
+  test('AFTER App component is unmounted, timer should be cleaned up  ', () => {
+    
+    //ARRANGE
+    jest.spyOn(global, 'clearInterval');
+
+    //ACT
+    const {unmount } = render(<App />)
+    //start timer
+    userEvent.click(screen.getByRole('button', { name: /start/i }))
+    //for 5 secs
+    act(() => {
+      jest.advanceTimersByTime(5000);
+    });
+    //make sure timer has not been cleared yet
+    expect(clearInterval).not.toHaveBeenCalled();
+    //unmount App component
+    unmount();
+
+    //ASSERT timer has been cleared on unmount
+    expect(clearInterval).toHaveBeenCalledTimes(1);
+
+    
+  });
+
+})
 
 // test('renders learn react link', () => {
 //   render(<App />);
@@ -17,61 +209,61 @@ import userEvent from '@testing-library/user-event';
 //   expect(sum(2,4)).toBe(6);
 // })
 
-describe('App tests suite', () => {
+// describe('App tests suite', () => {
 
-  test('renders App component', async () => {
+//   test('renders App component', async () => {
 
-    render(<App />);
+//     render(<App />);
 
-    // expect(screen.getByText('Search')).toBeInTheDocument();
+//     // expect(screen.getByText('Search')).toBeInTheDocument();
 
-    // expect(screen.getByText('Search:')).toBeInTheDocument();
+//     // expect(screen.getByText('Search:')).toBeInTheDocument();
 
-    // expect(screen.getByText(/Search/)).toBeInTheDocument();
+//     // expect(screen.getByText(/Search/)).toBeInTheDocument();
 
-    // expect(screen.getByRole('textbox')).toBeInTheDocument();
+//     // expect(screen.getByRole('textbox')).toBeInTheDocument();
 
-    // expect(screen.getByLabelText(/Search/)).toBeInTheDocument();
-    // expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
+//     // expect(screen.getByLabelText(/Search/)).toBeInTheDocument();
+//     // expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
 
-    // expect(screen.queryByText('Search')).toBeNull();
+//     // expect(screen.queryByText('Search')).toBeNull();
 
-    // expect(screen.queryByText(/Nabeel/)).not.toBeInTheDocument()
+//     // expect(screen.queryByText(/Nabeel/)).not.toBeInTheDocument()
 
-    // screen.debug();
+//     // screen.debug();
 
-    // expect(await screen.findByText(/Nabeel/)).toBeInTheDocument()
+//     // expect(await screen.findByText(/Nabeel/)).toBeInTheDocument()
 
-    // screen.debug();
+//     // screen.debug();
 
-    // For any element that isn't there yet but will be there eventually, use findBy over getBy or queryBy. 
-    // If you assert for a missing element, use queryBy. Otherwise default to getBy.
+//     // For any element that isn't there yet but will be there eventually, use findBy over getBy or queryBy. 
+//     // If you assert for a missing element, use queryBy. Otherwise default to getBy.
 
-    await screen.findByText(/Signed in as :/)
+//     await screen.findByText(/Signed in as :/)
 
-    // screen.debug();
-    expect(screen.queryByText(/Results for Javascript/)).toBeNull();
-
-
-    // fireEvent.change(screen.getByRole('textbox'), {
-    //   target: {
-    //     value: 'Javascript'
-    //   }
-    // })
-
-    userEvent.type(screen.getByRole('textbox'), 'Javascript')
+//     // screen.debug();
+//     expect(screen.queryByText(/Results for Javascript/)).toBeNull();
 
 
-    // screen.debug();
-    expect(screen.getByText(/Results for Javascript/)).toBeInTheDocument();
-  });
+//     // fireEvent.change(screen.getByRole('textbox'), {
+//     //   target: {
+//     //     value: 'Javascript'
+//     //   }
+//     // })
+
+//     userEvent.type(screen.getByRole('textbox'), 'Javascript')
 
 
-  test('renders App component', async () => {
+//     // screen.debug();
+//     expect(screen.getByText(/Results for Javascript/)).toBeInTheDocument();
+//   });
 
-    render(<App />);
-    await screen.findByText(/Signed in as :/)
 
-    expect(screen.getByRole('heading')).toBeInTheDocument()
-  });
-})
+//   test('renders App component', async () => {
+
+//     render(<App />);
+//     await screen.findByText(/Signed in as :/)
+
+//     expect(screen.getByRole('heading')).toBeInTheDocument()
+//   });
+// })
--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,99 @@
-import React, { ChangeEventHandler, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { idText } from 'typescript';
 import './App.css';
 
-const getUser = () =>
-{
-  return Promise.resolve({id:1,name:'Nabeel'})
-}
 
-function App() {
-  const title = 'Hello React :)'
-  const [value, setValue] = useState('')
-  const [user, setUser] = useState('');
+const App = () => {
+  const [startDisabled, setStartDisabled] = useState(false)
+  const [stopDisabled, setStopDisabled] = useState(true)
+  const [mins, setMins] = useState<number>(25);
+  const [secs, setSecs] = useState<number>(0);
+  // const [startTimer, setStartTimer] = useState(false);
 
+  // const zeroPad = (num: number, places: number) => String(num).padStart(places, '0')
+  let timer: NodeJS.Timeout;
+  const toggleButtons = () => {
+    setStartDisabled(!startDisabled);
+    setStopDisabled(!stopDisabled);
+  }
 
-  useEffect(()=>{
-    const loadUser = async () => {
-      const user = await getUser()
-      setUser(user.name);
+  useEffect(() => {
+    // console.log('start effect:');
+
+    return () => {
+      if (timer) {
+        // console.log('clear timer', startTimer)
+        clearTimer();
+      }
     }
-    loadUser();
-  },[])
 
+  }, [])
 
-  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
+  const clearTimer = () => {
+    if (timer) {
+      clearInterval(timer);
+    }
+  }
+
+  const resetTimer = () => {
+    clearTimer();
+    setSecs(0);
+    setMins(0);
+    setStartDisabled(false);
+    setStopDisabled(true);
+  }
+
+  const startTimer = () => {
+    clearTimer();
+
+    timer = setInterval(() => {
+      //Due to closures SetInterval keeps referencing to old values
+      setSecs(prevSecs => {
+        if (prevSecs === 0) {
+          //decrement minute value
+          setMins(prevMins => {
+            if(prevMins === 0) {
+              resetTimer();
+              return 0;
+            }
+            return prevMins - 1
+          });             
+          //set secs back to 59
+          return 59;
+        } else {
+          //othereise keep decrementing secs until 0
+          return prevSecs - 1
+        }
+      });
+    }, 1000);
+  }
+
+  const onStartHandler = (e: React.MouseEvent<HTMLElement>) => {
+    toggleButtons();
+    startTimer();
+  }
+
+  const onStopHandler = (e: React.MouseEvent<HTMLElement>) => {
+    toggleButtons();
+  }
+
+  const onResetHandler = (e: React.MouseEvent<HTMLElement>) => {
+    setStartDisabled(false);
+    setStopDisabled(true);
+
   }
 
   return (
     <div>
-      <h1>{title}</h1>
-      <div>
-        {user? <p>Signed in as : {user}</p> : null}
-      </div>
-
-      <Search value={value} onChange={onChangeHandler} >
-        Search:
-      </Search>
-
-      <div>Results for {value? value : '...'}</div>
+      <h1>Welcome nabeel!</h1>
+      <div role='timer'>{String(mins).padStart(2, '0')}:{String(secs).padStart(2, '0')}</div>
+      <button aria-label='Start' onClick={onStartHandler} disabled={startDisabled}>Start</button>
+      <button aria-label='Stop' onClick={onStopHandler} disabled={stopDisabled} >Stop</button>
+      <button aria-label='Reset' onClick={onResetHandler} >Reset</button>
 
     </div>
   );
 }
 
-type SearchProps = {
-  value: string,
-  onChange: ChangeEventHandler<HTMLInputElement>;
-}
-
-const Search: React.FC<SearchProps> = ({ value, onChange, children }) => {
-  return (
-    <div>
-      <label htmlFor='search'>{children}</label>
-      <input id='search' type='text' placeholder='Search' value={value} onChange={onChange} />
-    </div>
-  );
-}
 
 export default App;
